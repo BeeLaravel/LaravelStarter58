c6eedb49e45a5a16e6a22a6ffaade48525e6b5fd
BeeSoftEC <beherochuling@163.com>
2019-08-09 19:17:33 +0800

    beesoft

--- routes/test.php
+++ routes/test.php
@@ -4,4 +4,8 @@ Route::get('system/index', 'SystemController@index')->name('test.system.index');
 Route::get('soap/soap', 'SoapController@soap')->name('test.soap.soap');
 Route::get('soap/goodcang', 'SoapController@goodcang')->name('test.soap.goodcang');
 
+// Symfony
 Route::get('symfony/workflow', 'Symfony\WorkflowController@index')->name('test.symfony.workflow');
+
+// 资源
+Route::get('resource/lattice', 'Resource\LatticeController@index')->name('test.resource.lattice'); // jc91715/lattice 点阵图
